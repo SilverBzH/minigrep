16c4d683857c7eff33a1e1f2547ac8eec742b7dd
Silver <charlie.boutier7@gmail.com>
2020-03-22 00:34:23 +0100

    using iteratos instead of accessing directly the slice

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough argument bb");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didnt get a file name"),
+        };
+        
         let case_sensitive = env::var("CASE_SENSITIVE").is_err();
 
         Ok(Config {query, filename, case_sensitive})
